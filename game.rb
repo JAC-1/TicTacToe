4fc9ccb69cb05f21e9cde29bca44a4d977724514
JAC-1 <79427501+JAC-1@users.noreply.github.com>
2021-11-17 17:28:10 +0900

    Added multiple win conditions

--- game.rb
+++ game.rb
@@ -9,15 +9,19 @@ module GetMove
   end
 end
 
+module Won
+end
+
 class Game
   include GetMove
   @@board = [
     ["X", "Y", "X"],
-    ["X", "Y", "Y"],
-    [" ", " ", " "],
+    ["X", "X", "Y"],
+    [" ", " ", "X"],
   ]
 
-  def win?()
+  def winHorizontal?()
+    # Check each row. If there are three Xs or three Ys win.
     @@board.each do |line|
       xCount = 0
       yCount = 0
@@ -39,6 +43,78 @@ class Game
       end
     end
     return false
+  end
+
+  def winDiagonal?()
+    # [0][0] [1][1] [2][2]
+    # [0][2] [1][1] [2][0]
+    cY = 0
+    cX = 0
+    e = @@board.length
+    # Check from left to right
+    (0..e).each do |n|
+      if @@board[n][n] == "Y"
+        cY += 1
+      elsif @@board[n][n] == "X"
+        cX += 1
+        # @@board[n][e - n]
+      end
+    end
+    case totalWin(cY, cX)
+    when true
+      return true
+    end
+    # Check from right to left
+    (0..e).each do |n|
+      case @@board[n][e - n]
+      when "Y"
+        cY += 1
+      when "X"
+        cX += 1
+      end
+      case totalWin?(cY, cX)
+      when true
+        return true
+      end
+    end
+  end
+
+  def totalWin?(countY, countX)
+    if countY == 3
+      puts "Y won!"
+      return "Y won!"
+    elsif countX == 3
+      puts "X won!"
+      return "X won!"
+    else
+      cY = 0
+      cX = 0
+    end
+  end
+
+  def winVertical?()
+    # [0][0] [1][0] [2][0]
+    # [0][1] [1][1] [2][1]
+    # [0][2] [1][2] [2][2]
+    cY = 0
+    cX = 0
+    e = @@board.length
+    (0..e).each do |n|
+      @@board.each do |r|
+        if r[n] == "Y"
+          cY += 1
+        elsif r[n] == "X"
+          cX += 1
+        end
+      end
+      case totalWin?(cY, cX)
+      when true
+        return true
+      else
+        cY = 0
+        cX = 0
+      end
+    end
   end
 
   def display()
@@ -79,9 +155,16 @@ puts "Player 1 what is your name?"
 name1 = gets.chomp
 player1 = Player.new(name1)
 
-while game1.win? != true
-  game1.display
+win = false
+while win != true
   puts "#{player1.name}'s move"
+  if game1.winHorizontal?()
+    puts "Game won!"
+    break
+  elsif game1.winVertical?()
+    puts "Game won!"
+    break
+  end
   player1Move = player1.placeMarker(game1.getMove())
   game1.display
 end
@@ -91,4 +174,3 @@ end
 #   - Switch back and forth between players until loop breaks
 #   - Handle invalid input
 #   - Set private and public appropriatly
-#   - Better way to handel player name?
